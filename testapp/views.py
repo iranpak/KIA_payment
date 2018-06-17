c382829bd01cb220a7c4c0086ac9abb5aa365465
MohammadHossein Iranpak <mh.iranpak1997@gmail.com>
2018-06-18 02:22:57 +0430

    see activities by admin test

--- testapp/views.py
+++ testapp/views.py
@@ -8,25 +8,30 @@ def register(request):
     template = loader.get_template('testapp/register.html')
     return HttpResponse(template.render(context, request))
 
+
 def contact_us(request):
     context = {}
     template = loader.get_template('testapp/contact_us.html')
     return HttpResponse(template.render(context, request))
+
 
 def add_feature(request):
     context = {}
     template = loader.get_template('testapp/add_feature.html')
     return HttpResponse(template.render(context, request))
 
+
 def currency_to_rial(request):
     context = {}
     template = loader.get_template('testapp/currency_to_rial.html')
     return HttpResponse(template.render(context, request))
 
+
 def get_service_price(request):
     context = {}
     template = loader.get_template('testapp/get_service_price.html')
     return HttpResponse(template.render(context, request))
+
 
 # login
 def user_login(request):
@@ -34,14 +39,22 @@ def user_login(request):
     template = loader.get_template('testapp/user_login.html')
     return HttpResponse(template.render(context, request))
 
+
 def employee_login(request):
     context = {}
     template = loader.get_template('testapp/employee_login.html')
     return HttpResponse(template.render(context, request))
 
+
 def admin_login(request):
     context = {}
     template = loader.get_template('testapp/admin_login.html')
+    return HttpResponse(template.render(context, request))
+
+
+def admin_panel(request):
+    context = {}
+    template = loader.get_template('testapp/admin_panel.html')
     return HttpResponse(template.render(context, request))
 
 
