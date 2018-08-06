03a56e5014af7ea4108c60b9970ff11491f37b9e
MohammadHossein Iranpak <mh.iranpak1997@gmail.com>
2018-08-06 17:49:38 +0430

    models and fomrs of sign in and sign up

---
+++ KIA_auth/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+    account_number = forms.CharField()
+    phone_number = forms.CharField(max_length=11)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'password1', 'password2', 'email', 'account_number',
+                  'phone_number')
