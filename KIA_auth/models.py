fdf8aace9efe0af14063ac0179c0fb22c2995d6c
MohammadHossein Iranpak <mh.iranpak1997@gmail.com>
2018-08-06 17:48:07 +0430

    sign in and sign up added

--- KIA_auth/models.py
+++ KIA_auth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    role = models.CharField(max_length=16, default='user')
+    account_number = models.CharField(max_length=16)
+    balance = models.IntegerField(default=0)
+    phone_number = models.CharField(max_length=11)
+    is_restricted = models.BooleanField(default=False)
