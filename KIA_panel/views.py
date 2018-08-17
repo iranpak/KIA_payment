d4dbb65431666ecb349ce2f8282c654fede21a9c
MohammadReza Karegar <karegar@gmail.com>
2018-08-17 21:56:45 +0430

    added admin panel basics

---
+++ KIA_panel/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+
+
+def admin_panel(request):
+    context = {}
+    template = loader.get_template('KIA_panel/admin_panel.html')
+    return HttpResponse(template.render(context, request))
