a66ec880826e5f9543d656c9639a68908b3a171e
MohammadHossein Iranpak <mh.iranpak1997@gmail.com>
2018-08-24 12:11:10 +0430

    send email by employee edited

--- KIA_notification/views.py
+++ KIA_notification/views.py
@@ -19,7 +19,9 @@ def send_email_by_employee(request):
                 subject = request.POST.get('subject')
                 message_body = request.POST.get('message_body')
                 sender_address = 'kiapayment2018@gmail.com'
-                receiver_addresses = [request.POST.get('receiver_address')]
+                receiver_username = request.POST.get('receiver_username')
+                receiver_user = User.objects.get(username=receiver_username)
+                receiver_addresses = [receiver_user.email]
                 send_mail(subject, message_body, sender_address, receiver_addresses)
         else:
             return render(request, access_denied_template)
